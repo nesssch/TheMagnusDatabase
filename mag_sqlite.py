864ac74bf4ddf98444ee7cfce6a95e94ca54e7c5
nesssch <v.schattman@gmail.com>
2024-08-28 21:57:41 -0400

    fixes multiline string formatting for m2m fns

--- mag_sqlite.py
+++ mag_sqlite.py
@@ -21,12 +21,10 @@ def execute_query(connection, query):
         print(f"The error '{e}' occurred")
 
 def many_to_many(name, table1, table2):
-    return ('CREATE TABLE IF NOT EXISTS', name, '('
-        'id INTEGER PRIMARY KEY AUTOINCREMENT,'
-        + table1 + '_id INT NOT NULL,'
-        + table2 + '_id INT NOT NULL,'
-        'FOREIGN KEY (' + table1 + '_id REFERENCES ' + table1 + '(id),'
-        'FOREIGN KEY (' + table2 + '_id REFERENCES ' + table2 + '(id),'
+    return ('CREATE TABLE IF NOT EXISTS '+ name + ' (' 
+        'id INTEGER PRIMARY KEY AUTOINCREMENT,' + table1 + '_id INT NOT NULL,' + table2 + '_id INT NOT NULL,' 
+        'FOREIGN KEY (' + table1 + '_id) REFERENCES ' + table1 + '(id),' 
+        'FOREIGN KEY (' + table2 + '_id) REFERENCES ' + table2 + '(id),' 
     ');')
 
 connection = create_connection("test.db")
@@ -75,7 +73,9 @@ NAME_TO_QUERY = {
     "create_stat_char_table": many_to_many("statements_to_characters", "statements", "characters")
 }
 
+print(many_to_many("org_to_char", "organisations", "characters"))
+
 for query in NAME_TO_QUERY.values():
     execute_query (connection, query)
 
-# print(m2m("org_to_char", "organisations", "characters"))
+
