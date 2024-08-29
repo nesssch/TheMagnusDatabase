2844dda8ee3ba542251214510282aada57a84081
nesssch <v.schattman@gmail.com>
2024-08-28 21:59:22 -0400

    i lied earlier, this one actually fixes the formatting

--- mag_sqlite.py
+++ mag_sqlite.py
@@ -24,7 +24,7 @@ def many_to_many(name, table1, table2):
     return ('CREATE TABLE IF NOT EXISTS '+ name + ' (' 
         'id INTEGER PRIMARY KEY AUTOINCREMENT,' + table1 + '_id INT NOT NULL,' + table2 + '_id INT NOT NULL,' 
         'FOREIGN KEY (' + table1 + '_id) REFERENCES ' + table1 + '(id),' 
-        'FOREIGN KEY (' + table2 + '_id) REFERENCES ' + table2 + '(id),' 
+        'FOREIGN KEY (' + table2 + '_id) REFERENCES ' + table2 + '(id)' 
     ');')
 
 connection = create_connection("test.db")
