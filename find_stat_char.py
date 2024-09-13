eb98eaa7ca5d951e69185023d5b0a0d945875359
nesssch <v.schattman@gmail.com>
2024-09-13 17:12:41 -0400

    Basic sorting/formatting of char-to-ep information

--- find_stat_char.py
+++ find_stat_char.py
@@ -1,10 +1,21 @@
-f = open("stat_char.txt", "r")
-for i in range(20):
-print(f.readline())
-curr = f.readline()
-while curr != "Episode 2":
-    if curr[:2] != "Ep" and curr != "":
-        print (curr)
-    curr = f.readline()
+f = open("stat_char.txt", "r", encoding="utf8")
 
+def format_line(line:str):
+    return
+
+def get_ep_1():
+    test_arr = []
+    curr = f.readline()
+    count = 0
+    while curr != "Episode 2\n":
+        if curr[:2] != "Ep" and curr != "\n":
+            print (curr)
+            count += 1
+            test_arr.append(curr)
+        curr = f.readline()
+    return test_arr
+
+print(get_ep_1())
+# lines = f.readlines()
+# print(lines[:20])
 f.close()
